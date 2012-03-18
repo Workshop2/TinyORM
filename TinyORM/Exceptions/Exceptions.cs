42368f146c0461f7a2ce09dfda43f7dd39691a6e
Simon Colmer <Simon_Colmer@hotmail.com>
2012-03-18 12:46:40 +0000

    Sorting out exception/error handling

---
+++ TinyORM/Exceptions/Exceptions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TinyORM.Exceptions
+{
+    public class TinyMapperException : Exception
+    {
+        public TinyMapperException() { }
+        public TinyMapperException(string message){}
+        public TinyMapperException(string message, Exception inner) { }
+
+        protected TinyMapperException(System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context) { }
+    }
+    public class TinyDbException : Exception {
+        public TinyDbException() { }
+        public TinyDbException(string message){}
+        public TinyDbException(string message, Exception inner) { }
+
+        protected TinyDbException(System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context) { }
+    }
+}
